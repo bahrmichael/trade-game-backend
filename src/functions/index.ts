12d09a0db6b70693aed21de9903ca821c926c34d
Michael Bahr <michael@bahr.dev>
2022-11-19 00:51:58 +0100

    chore: add functions to stack

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,3 +1,5 @@
 export { default as hello } from './hello';
 export { default as secureHello } from './secure-hello';
 export { default as openapi } from './openapi';
+export { default as createApiKeyStart } from './createApiKeyStart';
+export { default as createApiKeyFinish } from './createApiKeyFinish';
