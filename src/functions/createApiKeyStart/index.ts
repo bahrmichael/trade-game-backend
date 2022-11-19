3df4fe67fcac10543e046e336e0ebbf80c783577
Michael Bahr <michael@bahr.dev>
2022-11-19 11:41:58 +0100

    fix: use correct env syntax

--- src/functions/createApiKeyStart/index.ts
+++ src/functions/createApiKeyStart/index.ts
@@ -2,7 +2,7 @@ import { handlerPath } from '@libs/handler-resolver';
 
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
-  environmentVariables: {
+  environment: {
     CLIENT_ID: '${self:custom.discordClientId}',
     REDIRECT_URL: "${self:custom.domain}/${self:provider.stage}/api-key/finish",
     TABLE: { Ref: 'AuthStateTable' }
