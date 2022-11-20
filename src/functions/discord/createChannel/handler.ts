aa843024b5f416fa8af6e63c7d2d140f78fc6020
Michael Bahr <michael@bahr.dev>
2022-11-20 17:18:54 +0100

    chore: more logging

--- src/functions/discord/createChannel/handler.ts
+++ src/functions/discord/createChannel/handler.ts
@@ -8,6 +8,7 @@ const {VERSION, GUILD_ID} = process.env;
 
 async function getSecret() {
   const secretResponse = await ssm.send(new GetSecretValueCommand({SecretId: 'discord_bot_secret'}))
+  console.log(secretResponse)
   return secretResponse.SecretString
 }
 
@@ -56,28 +57,14 @@ export const main = async (event: CloudFormationCustomResourceEvent, context: an
     await sendResponse(event, context, "SUCCESS", {});
   } catch (e) {
     console.error(e)
-    await sendResponse(event, context, "FAILED", {}, e);
+    await sendResponse(event, context, "FAILED", { error: e }, e);
   }
 };
 
 // https://aws.plainenglish.io/simple-example-of-lambda-backed-custom-resource-in-aws-cloudformation-6cf2f9f1a101
 // https://www.alexdebrie.com/posts/cloudformation-custom-resources/
 async function sendResponse(event, context, responseStatus: 'FAILED' | 'SUCCESS', responseData, e?) {
-  const responseBody = JSON.stringify({
-    Status: responseStatus,
-    Reason:
-        "See the details in CloudWatch Log Stream: " + context.logStreamName,
-    PhysicalResourceId: context.logStreamName,
-    StackId: event.StackId,
-    RequestId: event.RequestId,
-    LogicalResourceId: event.LogicalResourceId,
-    Data: responseData,
-  });
-
-  console.log("RESPONSE BODY:\n", responseBody);
-
   console.log("SENDING RESPONSE...\n");
-
   await axios.put(event.ResponseURL, {
     Status: responseStatus,
     Reason:
