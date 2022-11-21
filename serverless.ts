e794fe87c788345809874fe79ea469690514a9c1
Michael Bahr <michael@bahr.dev>
2022-11-21 14:20:01 +0100

    feat: resolve domain dynamically

--- serverless.ts
+++ serverless.ts
@@ -32,7 +32,7 @@ const serverlessConfiguration: AWS = {
   package: { individually: true },
   custom: {
     // later replace with a shared URL like https://api.tradegame.dev
-    domain: 'https://st1mnt1acj.execute-api.us-east-1.amazonaws.com',
+    domain: { "Ref" : "ApiGatewayRestApi" },
     esbuild: {
       bundle: true,
       minify: false,
