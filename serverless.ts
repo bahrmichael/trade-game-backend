ca9ee8f2972cde64101509619bc7dfb7e372deb2
Michael Bahr <michael@bahr.dev>
2022-11-21 14:10:41 +0100

    chore: update domain

--- serverless.ts
+++ serverless.ts
@@ -32,7 +32,7 @@ const serverlessConfiguration: AWS = {
   package: { individually: true },
   custom: {
     // later replace with a shared URL like https://api.tradegame.dev
-    domain: 'https://qx3ep8iubc.execute-api.us-east-1.amazonaws.com',
+    domain: 'https://yxiwbmqs5i.execute-api.us-east-1.amazonaws.com',
     discordClientId: '1043200977156714607',
     esbuild: {
       bundle: true,
