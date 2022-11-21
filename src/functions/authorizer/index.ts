b710a8a88bfc45ae53466834af282c44259614ca
Michael Bahr <michael@bahr.dev>
2022-11-21 13:27:05 +0100

    fix: remove import value calls to test if export works

--- src/functions/authorizer/index.ts
+++ src/functions/authorizer/index.ts
@@ -3,7 +3,7 @@ import {handlerPath} from '@libs/handler-resolver';
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
   environment: {
-    JWT_SECRET: {'Fn::ImportValue': 'JwtSecret'},
+    // JWT_SECRET: {'Fn::ImportValue': 'JwtSecret'},
   },
   tags: {
     function: 'authorizer'
