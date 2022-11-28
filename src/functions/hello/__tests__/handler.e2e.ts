6b9028a38b63b43167419c30dc5e790b877a2755
Michael Bahr <michael@bahr.dev>
2022-11-28 18:33:44 +0100

    chore: use .env variables to make api call in test

--- src/functions/hello/__tests__/handler.e2e.ts
+++ src/functions/hello/__tests__/handler.e2e.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const {BASE_URL} = process.env;
+const {API_ID, VERSION} = process.env;
+
+const BASE_URL = `https://${API_ID}.execute-api.us-east-1.amazonaws.com/${VERSION}`
 
 it('should receive a hello message', async() => {
     const result = await axios.post(`${BASE_URL}/hello`, { name: 'Michael' })
