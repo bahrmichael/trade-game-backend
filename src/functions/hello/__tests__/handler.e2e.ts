60372e4daf78ecb323af45b79ff8b43f0101b699
Michael Bahr <michael@bahr.dev>
2022-11-28 20:11:07 +0100

    fix: let test access message field

--- src/functions/hello/__tests__/handler.e2e.ts
+++ src/functions/hello/__tests__/handler.e2e.ts
@@ -2,5 +2,5 @@ import client from "../../../libs/__tests__/axios";
 
 it('should receive a hello message', async() => {
     const result = await client.post(`/hello`, { name: 'Michael' })
-    expect(result.data).toBe('Hello Michael, welcome to the exciting Serverless world!')
+    expect(result.data.message).toBe('Hello Michael, welcome to the exciting Serverless world!')
 })
