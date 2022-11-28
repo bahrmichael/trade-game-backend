e9dd70a805da5514dab09c0f21eb506473b747e9
Michael Bahr <michael@bahr.dev>
2022-11-28 20:49:25 +0100

    chore: add secured test

---
+++ src/functions/secure-hello/__tests__/handler.e2e.ts
@@ -0,0 +1,70 @@
+import {ddb} from "../../../libs/ddb-client";
+import {QueryCommand} from "@aws-sdk/lib-dynamodb";
+import client from "../../../libs/__tests__/axios";
+
+const {TEST_TOKEN_TABLE} = process.env;
+
+function delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
+
+describe('secure-hello', () => {
+
+    let token;
+
+    beforeAll(async() => {
+
+        const tokenRecords = await ddb.send(new QueryCommand({
+            TableName: TEST_TOKEN_TABLE,
+            KeyConditionExpression: 'pk = :k',
+            FilterExpression: 'visibleFrom > :v and timeToLive > :v',
+            ExpressionAttributeValues: {
+                ':k': 'key',
+                ':v': new Date().getTime(),
+            },
+            ScanIndexForward: false,
+            Limit: 1,
+        }))
+        token = tokenRecords.Items[0]
+        if (!token) {
+            throw Error('Could not find a token in the table.');
+        }
+
+        // try for up to two minutes
+        const tryUntil = new Date(new Date().getTime() + 1_000 * 120).getTime()
+
+        let success = false;
+        while (tryUntil > new Date().getTime()) {
+            try {
+                // When first created, the token will need about 1 minute to become valid
+                // We're not waiting that time though, because this test might run when a token is already valid
+                await client.post(`/secure-hello`, {name: 'Michael'}, {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                })
+                success = true;
+                break;
+            } catch (e) {
+                console.warn(e);
+                console.log('Sleeping 20 seconds ...')
+                await delay(20_000);
+            }
+        }
+
+        if (!success) {
+            throw Error('Token did not initialize in time.')
+        }
+
+    }, 120_000)
+
+
+    it('should receive a hello message', async() => {
+        const result = await client.post(`/hello`, { name: 'Michael' }, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        expect(result.data.message).toBe('Hello Michael, welcome to the exciting Serverless world!')
+    })
+})
