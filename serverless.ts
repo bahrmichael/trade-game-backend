ac8e925ce0c52c1bb7e5c75c3a20b122dd991afd
Michael Bahr <michael@bahr.dev>
2022-11-28 21:10:29 +0100

    chore: introduce dedicated role for test runs

--- serverless.ts
+++ serverless.ts
@@ -185,6 +185,42 @@ const serverlessConfiguration: AWS = {
           ServiceToken : { 'Fn::GetAtt': ['InitTestKeyLambdaFunction', 'Arn' ] },
         },
       },
+      TestUserRole: {
+        Type: 'AWS::IAM::Role',
+        Properties: {
+          RoleName: '${self:service}-${self:provider.stage}-TestUserRole',
+          AssumeRolePolicyDocument: {
+            Statement: [{
+              Effect: 'Allow',
+              Action: 'sts:AssumeRoleWithWebIdentity',
+              Principal: {
+                Federated: 'arn:aws:iam::${aws:accountId}:oidc-provider/token.actions.githubusercontent.com'
+              },
+              Condition: {
+                StringLike: {
+                  'token.actions.githubusercontent.com:sub': 'repo:bahrmichael/trade-game-backend:*'
+                },
+                'ForAllValues:StringEquals': {
+                  'token.actions.githubusercontent.com:iss': 'https://token.actions.githubusercontent.com',
+                  'token.actions.githubusercontent.com:aud': 'sts.amazonaws.com',
+                  'token.actions.githubusercontent.com:actor': ['bahrmichael', 'renovate[bot]', 'dependabot[bot]', 'mergify[bot]']
+                }
+              }
+            }]
+          },
+          Policies: [{
+            PolicyName: 'TestUserPolicy',
+            PolicyDocument: {
+              Version: '2012-10-17',
+              Statement: [{
+                Effect: 'Allow',
+                Action: ['dynamodb:Query'],
+                Resource: [{Ref: 'TestKeyTable'}]
+              }]
+            }
+          }]
+        }
+      }
     }
   }
 };
