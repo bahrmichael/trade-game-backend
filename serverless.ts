1d12c2ce7f6f7d902f8f0aba306217542182a73d
Michael Bahr <michael@bahr.dev>
2022-12-08 13:13:38 +0100

    chore: use subdomains for versions

--- serverless.ts
+++ serverless.ts
@@ -6,7 +6,7 @@ import * as postHelloSchema from "@functions/hello/schema"
 const serverlessConfiguration: AWS = {
   service: 'trade-game-backend',
   frameworkVersion: '3',
-  plugins: ['serverless-esbuild', 'serverless-plugin-log-retention', 'serverless-iam-roles-per-function', '@motymichaely/serverless-openapi-documentation'],
+  plugins: ['serverless-esbuild', 'serverless-plugin-log-retention', 'serverless-iam-roles-per-function', '@motymichaely/serverless-openapi-documentation', 'serverless-domain-manager'],
   provider: {
     name: 'aws',
     runtime: 'nodejs16.x',
@@ -32,10 +32,17 @@ const serverlessConfiguration: AWS = {
   functions,
   package: { individually: true },
   custom: {
-    // later replace with a shared URL like https://api.tradegame.dev
-    domain: 'https://api.apiempires.com',
+    domain: 'https://${self:provider.stage}.api.apiempires.com',
     discordClientId: '1043200977156714607',
     discordRedirectUrl: '${self:custom.domain}/${self:provider.stage}/api-key/finish',
+    customDomain: {
+      domainName: '${self:provider.stage}.api.apiempires.com',
+      basePath: '${self:provider.stage}',
+      certificateName: '*.api.apiempires.com',
+      stage: '${self:provider.stage}',
+      createRoute53Record: true,
+      autoDomain: true,
+    },
     esbuild: {
       bundle: true,
       minify: false,
